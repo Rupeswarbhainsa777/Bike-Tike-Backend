aa6efb88d5d86d7b23e61d02154fde9707ee6e60
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-01-23 12:35:16 +0530

    Mode created

---
+++ src/main/java/com/code/Bike/Model/Bike.java
@@ -0,0 +1,49 @@
+package com.code.Bike.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "bike")
+public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String brand;
+
+    private String image;   // image URL or path
+
+    private Long price;
+
+    private String colour;
+
+    private String mileage;
+
+    private Integer engineCc;
+
+    private Integer topSpeed;
+
+    private String transmission; // Manual / Automatic
+
+    private Integer fuelTankCapacity;
+
+    private Integer yearOfLaunch;
+
+    @Enumerated(EnumType.STRING)
+    private BikeType bikeType;
+
+    @Enumerated(EnumType.STRING)
+    private FuelType fuelType;
+
+    @Enumerated(EnumType.STRING)
+    private BikeStatus status;
+}
---
+++ src/main/java/com/code/Bike/Model/BikeType.java
@@ -0,0 +1,8 @@
+package com.code.Bike.Model;
+
+public enum BikeType {
+    SPORTS,
+    CRUISER,
+    COMMUTER,
+    ELECTRIC
+}
