791bd9e19eb5cf81a8090a81f2078e23e662178e
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-01-24 19:53:08 +0530

    add api created

--- src/main/java/com/code/Bike/Controller/BikeController.java
+++ src/main/java/com/code/Bike/Controller/BikeController.java
@@ -1,15 +1,20 @@
 package com.code.Bike.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.code.Bike.Model.Bike;
+import com.code.Bike.Service.BikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bike")
 public class BikeController {
 
-    @GetMapping("/hi")
-    public String hello(){
-        return "Hii";
-    }
+    @Autowired
+    private BikeService bikeService;
+
+    @PostMapping("/add")
+   public Bike add(@RequestBody Bike bike){
+       return bikeService.saveBike(bike);
+   }
 }
--- src/main/java/com/code/Bike/Service/BikeService.java
+++ src/main/java/com/code/Bike/Service/BikeService.java
@@ -1,9 +1,19 @@
 package com.code.Bike.Service;
 
 
+import com.code.Bike.Model.Bike;
+import com.code.Bike.Repository.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BikeService {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
+    public Bike saveBike(Bike bike){
+        return bikeRepository.save(bike);
+    }
 
 }
