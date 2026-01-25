ac8cd7c7b24980ab52379c963e810b88b8330dfd
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-01-25 19:17:25 +0530

    get api added on bike controller

--- src/main/java/com/code/Bike/Controller/BikeController.java
+++ src/main/java/com/code/Bike/Controller/BikeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/bike")
 public class BikeController {
@@ -15,6 +17,12 @@ public class BikeController {
 
     @PostMapping("/add")
    public Bike add(@RequestBody Bike bike){
-       return bikeService.saveBike(bike);
+        return bikeService.saveBike(bike);
    }
+
+   @GetMapping("/all")
+   public ResponseEntity<List<Bike>> getAll(){
+        return bikeService.getAlll();
+   }
+
 }
