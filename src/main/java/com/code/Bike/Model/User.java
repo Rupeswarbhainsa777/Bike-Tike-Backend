93702f45ce7bf0cb4dff26b219c9467430b5cbd5
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-05 20:56:56 +0530

    user modify

--- src/main/java/com/code/Bike/Model/User.java
+++ src/main/java/com/code/Bike/Model/User.java
@@ -1,4 +1,19 @@
 package com.code.Bike.Model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="tbl_user")
 public class User {
+
 }
