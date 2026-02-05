f872d1267548abf818b1736af9b266113685cba7
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-05 21:00:40 +0530

    all updated

--- src/main/java/com/code/Bike/Model/User.java
+++ src/main/java/com/code/Bike/Model/User.java
@@ -1,19 +1,39 @@
 package com.code.Bike.Model;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
 
 @Data
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name ="tbl_user")
+@Table(name = "tbl_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String userId;
+
+    private String email;
+    private String password;
+    private String role;
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
