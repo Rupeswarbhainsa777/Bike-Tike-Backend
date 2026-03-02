a91d07c5ad585b8f41e26355afdcdc36c9b26ebb
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-02 22:14:49 +0530

    securiy config updated

--- src/main/java/com/code/Bike/config/SecurityConfig.java
+++ src/main/java/com/code/Bike/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.code.Bike.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ public class SecurityConfig {
         security
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
