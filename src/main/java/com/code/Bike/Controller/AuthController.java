0da0bb704a37af23e63442873a40e36776448116
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-02 22:40:22 +0530

    user add api created

--- src/main/java/com/code/Bike/Controller/AuthController.java
+++ src/main/java/com/code/Bike/Controller/AuthController.java
@@ -2,17 +2,24 @@ package com.code.Bike.Controller;
 
 
 import com.code.Bike.Model.User;
+import com.code.Bike.Service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class AuthController {
 
-    public User addUser(@RequestBody User user){
+    @Autowired
+    private UserService userService;
 
+
+
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user){
+         return userService.adduser(user);
     }
 }
--- src/main/java/com/code/Bike/Service/UserService.java
+++ src/main/java/com/code/Bike/Service/UserService.java
@@ -1,7 +1,17 @@
 package com.code.Bike.Service;
 
+import com.code.Bike.Model.User;
+import com.code.Bike.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public User adduser(User user){
+        return userRepo.save(user);
+    }
 }
