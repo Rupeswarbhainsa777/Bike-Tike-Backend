27a5a9be58aa439a0d2caa05879a51bfe8350ae3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-02 22:28:10 +0530

    userrepo updated

--- src/main/java/com/code/Bike/Repository/UserRepo.java
+++ src/main/java/com/code/Bike/Repository/UserRepo.java
@@ -1,8 +1,10 @@
 package com.code.Bike.Repository;
 
+import com.code.Bike.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo {
+public interface UserRepo extends JpaRepository<User,Integer> {
 
 }
