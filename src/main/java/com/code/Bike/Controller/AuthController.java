d1bb77cb7a86f927fe72407d3597cc34aadfcfb3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-02 22:26:12 +0530

    userservice addedd

--- src/main/java/com/code/Bike/Controller/AuthController.java
+++ src/main/java/com/code/Bike/Controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.code.Bike.Controller;
 
 
+import com.code.Bike.Model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
 public class AuthController {
+
+    public User addUser(@RequestBody User user){
+
+    }
 }
